f8b6ce9c4791f5e0cb3246ea9fd2a24f33279253
filipegiusti <filipegiusti@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-26 21:57:40 +0000

    Testing namespace.
    git-svn-id: https://plugins.svn.wordpress.org/livepress-wp/trunk@177324 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ livepress.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: LivePress_WP
+Plugin URI:
+Description: Enables wordpress...
+Version: 0.3
+Author: Me
+Author URI: http://www.filipegiusti.com
+*/
+
+define('LIVEPRESS_WP', 'livepress_wp');
+
+?>
